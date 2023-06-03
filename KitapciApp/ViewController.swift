cdf48ceb37f7107f04c0255a03ca5ee4287290d2
Yusuf Kaan <yusufkaansoygenc@icloud.com>
2023-06-03 13:06:28 +0300

    Initial Commit

---
+++ KitapciApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KitapciApp
+//
+//  Created by Yusuf Kaan Soygenç on 3.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
